56f2a509eb1b76b07efa5a067226e89d0d211e07
Pavel <impetus@yandex.ru>
2020-11-06 17:07:18 +0300

    Initial commit

---
+++ src/song.kt
@@ -0,0 +1,17 @@
+/*
+Song description
+*/
+
+var songName: String = "Fade Into You"
+var artistName: String = "Mazzy Star"
+var year: Short? = 1983
+var size: Int = 46528233
+var length: Short = 180
+var original: Boolean = true
+
+Log.d("songName=",trackName)
+Log.d("artistName=",trackName)
+Log.d("year=",year.toString())
+Log.d("size=",size.toString())
+Log.d("length=",length.toString())
+Log.d("original=",original.toString())
